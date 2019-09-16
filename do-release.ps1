a02a90e0950a61f2d364e4d7437deb70017eeba7
Charles-Antoine Degennes <cadegenn@univ-lr.fr>
2019-09-16 12:33:35 +0200

    general commit before tag 1.0.0

--- do-release.ps1
+++ do-release.ps1
@@ -352,7 +352,7 @@ if ($CURRENT_BRANCH -eq "release/v$TAG") {
 }
 
 # comment every edebug(), edevel(), eenter() and eeleave() calls in every single file
-ForEach ($f in (Get-ChildItem -Path $Global:DIRNAME -Recurse -Include "*.psm1")) {
+ForEach ($f in (Get-ChildItem -Path $Global:DIRNAME -Recurse -Include "*.ps1")) {
 	(Get-Content $f.FullName) -replace "^(\s+)edebug", '$1# edebug' | Set-Content -Encoding UTF8 $f.FullName
 	(Get-Content $f.FullName) -replace "^(\s+)edevel", '$1# edevel' | Set-Content -Encoding UTF8 $f.FullName
 	(Get-Content $f.FullName) -replace "^(\s+)eenter", '$1# eenter' | Set-Content -Encoding UTF8 $f.FullName
